3c5b415cafb3e84a54a3952b7c4508542a91d51a
BashaVali <bashavali222@gmail.com>
2022-11-08 03:38:18 +0530

    Employee Wage Problem

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("Employee Wage Problem " + "\n\n");
+
+                Console.WriteLine(" Select the option ");
+
+                Console.WriteLine("1.Attendance" + "\n");
+
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        EmployeeWage employeeWage = new EmployeeWage();
+                        employeeWage.Attendence();
+                        break;
+                }
+            }
+        }
+    }
+}
